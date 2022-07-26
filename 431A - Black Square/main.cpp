ce44e2fb32dca38f258f92e379a0144206224650
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:16 +0200

    431A - Black Square

---
+++ 431A - Black Square/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    string s;
+    int cnt=0;
+    int a[4];
+    for (int i = 0; i < 4; ++i) {
+        cin>>a[i];
+    }
+    cin>>s;
+    for (int i = 0; i < s.length(); ++i) {
+        cnt+=a[(int)s[i]-48-1];
+    }
+    cout<<cnt;
+    return 0;
+}
+//(int)'0'==48
