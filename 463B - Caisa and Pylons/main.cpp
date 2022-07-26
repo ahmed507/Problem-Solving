3526976913e29465dbdffc59e195609a52b1457f
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:16 +0200

    463B - Caisa and Pylons

---
+++ 463B - Caisa and Pylons/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    // ios::sync_with_stdio(false);
+    // cin.tie(0);
+    // cout.tie(0);
+    int n,h,last=0,energy=0,dolars=0;
+    cin>>n;
+    for (int i = 0; i < n; i++){
+        cin>>h;
+        energy += last-h;
+        if(energy<0){
+            dolars+=abs(energy);
+            energy=0;
+        }
+        last = h;
+    }
+    cout<<dolars;
+
+    return 0;
+}
