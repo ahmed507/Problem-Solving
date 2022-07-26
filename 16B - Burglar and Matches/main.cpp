d927c53ed74ee83821d6f46f9506a94762acccf4
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:15 +0200

    16B - Burglar and Matches

---
+++ 16B - Burglar and Matches/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+string get_biggest(string s1,string s2){
+    if(s1.length()>=s2.length())return s1;
+    else return s2;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    unsigned int a,b,n,m,total=0;
+
+    cin>>n>>m;
+    vector<pair<unsigned int,unsigned int>>k(m);
+    for (int i = 0; i < m; ++i) {
+        cin>>a>>b;
+        auto &it = k.at(i);
+        it.first = a;
+        it.second=b;
+    }
+    std::sort(k.begin(), k.end(), [](auto &left, auto &right) {
+        return left.second < right.second;
+    });
+    for (int i = m-1; i > -1 && n>0; --i) {
+        if(n>k[i].first){
+            n-=k[i].first;
+            total+=((k[i].first)*(k[i].second));
+        } else {
+            total+=(n*(k[i].second));
+            n = 0;
+        }
+    }
+    cout<<total;
+
+
+
+    return 0;
+}
