3e6010fd230bd3359b32de32fed4aaa15ab31132
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:15 +0200

    231A - Team

---
+++ 231A - Team/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+//    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int n,a,b,c,cnt;
+    cnt = 0;
+    cin >> n;
+    for (int i = 0; i <n ; ++i) {
+        cin>>a>>b>>c;
+        if(a+b+c>=2)cnt+=1;
+    }
+    cout<<cnt;
+    return 0;
+
+}
