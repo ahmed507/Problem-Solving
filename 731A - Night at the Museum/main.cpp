d12a699ecd040e47d08f7e1e3e8108da2734a517
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:16 +0200

    731A - Night at the Museum

---
+++ 731A - Night at the Museum/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+//a = 97
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    string alphabets="abcdefghijklmnopqrstuvwxyz";
+    string s;
+    int left,right,cur=0,cnt=0;
+    cin>>s;
+    for (int i = 0; i < s.length(); ++i) {
+        right=0;
+        left=0;
+        for (int j = cur; ; ++j) {
+            if(j==26){
+                j= 0;
+            }
+            if (s[i]== alphabets[j])break;
+            ++right;
+        }
+        for (int j = cur;; --j) {
+            if(j==-1){
+                j= 25;
+            }
+
+            if (s[i]== alphabets[j]){
+                cur=j;
+                break;
+            }
+            ++left;
+
+        }
+        cnt+=min(left,right);
+    }
+    cout<<cnt;
+    return 0;
+}
