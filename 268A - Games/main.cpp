deacc6281c77156cb92a06a88fe71f3dd733b51c
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:15 +0200

    268A - Games

---
+++ 268A - Games/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n,h,g,cnt=0;
+    vector<int>home,guest;
+    cin>>n;
+    for (int i = 0; i < n; ++i) {
+        cin>>h;
+        cin>>g;
+        home.push_back(h);
+        guest.push_back(g);
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if(home[i]==guest[j])++cnt;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
