d5d20c50fb9f5f8f0caeeae6212948cb72df1f9e
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:17 +0200

    791A - Bear and Big Brother

---
+++ 791A - Bear and Big Brother/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+//    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int a,b,cnt;
+    cnt = 1;
+    cin>>a>>b;
+    while (b>=a){
+        a *= 3 *cnt;
+        b *= 2*cnt;
+        cnt+=1;
+    }
+    cout<<cnt-1;
+    return 0;
+}
