49786e70ff501a0d95679cb34336bf21d4f28cec
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:15 +0200

    266A - Stones on the Table

---
+++ 266A - Stones on the Table/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n,cnt=0;
+    char c,last='z';
+    cin>>n;
+    for (int i = 0; i < n; ++i) {
+        cin>>c;
+        if(c==last)cnt+=1;
+        last=c;
+    }
+    cout<<cnt;
+    return 0;
+}
