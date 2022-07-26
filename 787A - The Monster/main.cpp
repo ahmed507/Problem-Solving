611a9c650f3a82bd03df82efc4109ea35c6fd63e
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:16 +0200

    787A - The Monster

---
+++ 787A - The Monster/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool is_integer(float k)
+{
+    return std::floor(k) == k;
+}
+int sol(int a,int b,int c,int d){
+    for (double i = 0; i < 101; ++i) {
+        if(is_integer(((d+i*c)-b)/a) && ((d+i*c)-b)/a >=0)return ((d+i*c)-b)/a;
+    }
+    return -1;
+}
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    int a,b,c,d;
+    double n,m=0;
+    cin>>a>>b;
+    cin>>c>>d;
+    if(sol(a,b,c,d)!=-1)cout<<sol(a,b,c,d)*a+b;
+    else cout<<sol(a,b,c,d);
+    return 0;
+}
