c1f59736e8a028ef7a9ff1f872fffa29062eb363
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:16 +0200

    734A - Anton and Danik

---
+++ 734A - Anton and Danik/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+//    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    long int n,cnt_a,cnt_d;
+    cnt_a=0;
+    cnt_d=0;
+    string s;
+    cin>>n;
+    cin>>s;
+    for (int i = 0; i < s.length(); ++i) {
+        if(s[i]=='A')cnt_a+=1;
+        else cnt_d+=1;
+    }
+    if(cnt_a>cnt_d)cout<<"Anton";
+    else if(cnt_a<cnt_d) cout<<"Danik";
+    else cout<<"Friendship";
+    return 0;
+
+}
