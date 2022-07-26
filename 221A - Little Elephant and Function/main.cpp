2baba83e08e1769bb111d59f295a0f86eefd8cd0
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:15 +0200

    221A - Little Elephant and Function

---
+++ 221A - Little Elephant and Function/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int>a;
+
+int main() {
+    int x;
+    cin>>x;
+    cout<<x;
+    for (int i = 1; i <x; i++) {
+        cout<<" "<<i;
+    }
+    return 0;
+}
