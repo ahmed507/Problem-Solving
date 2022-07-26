0c99c238c0ae7667ad7f0216fcd57d57f0b49e17
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:16 +0200

    677A - Vanya and Fence

---
+++ 677A - Vanya and Fence/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+//    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int n,h,cnt;
+    cnt=0;
+    cin>>n>>h;
+    vector<int> a(n);
+    for(int &e :a){
+        cin>>e;
+    }
+    for(const auto &e :a){
+        if(e>h)cnt+=2;
+        else cnt+=1;
+    }
+    cout<<cnt;
+}
