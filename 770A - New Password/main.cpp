861d6b28913379f5ad43d91238b640f239c5ee41
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:16 +0200

    770A - New Password

---
+++ 770A - New Password/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    string letters = "abcdefghijklmnopqrstuvwxyz";
+    vector<char>password;
+    int n,k;
+    cin>>n>>k;
+    while(n>0){
+        for (int i = 0; i < k; i++)
+        {
+            password.push_back(letters[i]);
+            --n;
+            if(n==0)break;
+        }       
+        
+    }
+    for (int i = 0; i < password.size(); i++){
+        cout<<password[i];
+    }
+    
+    
+
+    return 0;
+}
