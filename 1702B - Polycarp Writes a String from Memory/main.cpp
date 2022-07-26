8cf895f7c7d90179ededed6ef393060628302020
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:15 +0200

    1702B - Polycarp Writes a String from Memory

---
+++ 1702B - Polycarp Writes a String from Memory/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+   cout.tie(0);
+   unsigned int t,cnt1,cnt2;
+   string s;
+   cin>>t;
+   while (t--){
+       vector<char>letters;
+       cnt2=0;
+       cnt1=0;
+       cin>>s;
+       for (int i = 0; i < s.length(); ++i) {
+           if(cnt1 == 3 && !(find(letters.begin(),letters.end(),s[i])!=letters.end())){
+               letters.clear();
+               ++cnt2;
+               cnt1 = 0;
+           }
+
+           if(!(find(letters.begin(),letters.end(),s[i])!=letters.end())) {
+               letters.push_back(s[i]);
+               ++cnt1;
+           }
+           if(i==s.length()-1 && cnt1>0)++cnt2;
+       }
+       cout<<cnt2<<'\n';
+   }
+
+   return 0;
+}
