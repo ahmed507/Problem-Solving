2b90e844b1df9f54f673cc308bbfb9c655fd9c3e
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:15 +0200

    427A - Police Recruits

---
+++ 427A - Police Recruits/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n,temp,cnt=0,police=0;
+    cin>>n;
+    for (int i = 0; i <n ; ++i) {
+        cin>>temp;
+        if(temp==-1 && police==0){
+            ++cnt;
+        }
+        else if(temp == -1 && police>0){
+            --police;
+        }
+        else{
+            police+=temp;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
