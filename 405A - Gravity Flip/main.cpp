708b3804d56d6a42a1e298418e578bb9d3b67ca1
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:15 +0200

    405A - Gravity Flip

---
+++ 405A - Gravity Flip/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+//    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int n,x;
+    vector<int>cubes;
+    cin>>n;
+    for (int i = 0; i <n ; ++i) {
+        cin>>x;
+        cubes.push_back(x);
+    }
+    sort(cubes.begin(), cubes.end());
+    for (int i = 0; i <n ; ++i) {
+        cout<<cubes[i]<<" ";
+    }
+    return 0;
+}
