55906e286ef3f749d4bd3eb18cf891a6cb98f1f6
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-07-26 20:22:15 +0200

    381A - Sereja and Dima

---
+++ 381A - Sereja and Dima/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n,x;
+    int sd[]={0,0};
+    vector<int>cards;
+    cin>>n;
+    for (int i = 0; i < n; ++i) {
+        cin >> x;
+        cards.push_back(x);
+    }
+
+    int k=0;
+    while (!cards.empty()){
+        if(k>1)k=0;
+        if(cards.front()>cards.back()){
+            sd[k]+=cards.front();
+            cards.erase(cards.begin());
+        }
+        else{
+            sd[k]+=cards.back();
+            cards.pop_back();
+        }
+        ++k;
+    }
+    cout<<sd[0]<<" "<<sd[1];
+    return 0;
+}
