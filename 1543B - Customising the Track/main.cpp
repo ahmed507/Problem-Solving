86f67c26025b80b42b5a325b86cb7c5a028d7852
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-08-02 01:22:16 +0200

    1543B - Customising the Track

--- 1543B - Customising the Track/main.cpp
+++ 1543B - Customising the Track/main.cpp
@@ -1,15 +1,7 @@
+//https://codeforces.com/problemset/problem/1543/B
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-//long get_incov(vector<ll> a){
-//    long c=0;
-//    for (ll i = 0; i < a.size(); ++i) {
-//        for (ll j = i+1; j < a.size(); ++j) {
-//            c+= abs(a[i]-a[j]);
-//        }
-//    }
-//    return c;
-//}
 int main() {
 //    ios::sync_with_stdio(false);
 //    cin.tie(0);
@@ -25,19 +17,10 @@ int main() {
             a.push_back(z);
             sum+=z;
         }
-//        avg=sum/a.size();
         q = sum%a.size();
-//        for (int j = 0; j < a.size(); ++j) {
-//            if(q>0){
-//                o.push_back(avg+1);
-//                --q;
-//            }
-//            else o.push_back(avg);
-//        }
 
         cout<<q*(n-q)<<'\n';
         a.clear();
-//        o.clear();
     }
 
 
