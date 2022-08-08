36a475d62c9bc49c6ad5f694d6c9658d9fedb879
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-08-08 18:24:03 +0200

    385B - Bear and Strings

--- 385B - Bear and Strings/main.cpp
+++ 385B - Bear and Strings/main.cpp
@@ -5,27 +5,28 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int n,k,tmp;
-    long long int sum=0;
-    vector<int>a;
-    cin>>n>>k;
-    for (int i = 0; i < n; i++){
-        cin>>tmp;
-        if(tmp<0 && k>0){
-            tmp*=-1;
-            --k;
+    string s;
+    vector<pair<int,int>>bear_loc;
+    long long int cnt = 0;
+    cin>>s;
+    if(s.length()<4){
+        cout<<0;
+        return 0;
+    }
+    for(int i = 0 ; i<s.length()-3;++i){
+        if(s[i]=='b'&&s[i+1]=='e'&&s[i+2]=='a'&&s[i+3]=='r') {
+            bear_loc.push_back(make_pair(i,i+3));
+            i = i+3;
         }
-        sum+=tmp;
-        a.push_back(tmp);
     }
-    
-    if(k==0)cout<<sum;
-    else{
-        sort(a.begin(), a.end());
-        if(k%2==0)cout<<sum;
-        else cout<<sum-2*a[0];
+    for (int i = 0; i < bear_loc.size(); ++i) {
+        if(i==0){
+            cnt+=(bear_loc[i].first+1)*(s.length()-bear_loc[i].second);
+        } else{
+            cnt+=(bear_loc[i].first-bear_loc[i-1].first)*(s.length()-bear_loc[i].second);
+        }
     }
-
+    cout<<cnt;
 
     return 0;
 }
